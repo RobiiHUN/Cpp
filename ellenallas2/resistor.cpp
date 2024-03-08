90639f3d7c1d8012a61b721bbeb848698390816c
RobiiiHUN <58343667+RobiiHUN@users.noreply.github.com>
2024-03-08 19:00:18 +0100

    03.08

---
+++ ellenallas2/resistor.cpp
@@ -0,0 +1,70 @@
+﻿/**
+ * \file resistor.cpp
+ *   (UTF-8 kodolasu fajl. Allitsa at a megjenetes kodolasat,
+ *    ha a tovabbi kommentek nem olvashatok helyesen!)
+ *
+ * Ohmikus ellenállást modellező osztály megvalósítása
+ */
+
+/**
+ * Itt kell megvalósítani a resistor.h-ban deklarált nem inline függvényeket.
+ * A Jportára ezt a fájlt kell feltölteni.
+ */
+
+#include "resistor.h"
+
+double alap_ertek = 43;
+
+#ifndef NO_STATIC
+void Resistor::setDef(double r)
+{
+    alap_ertek = r;
+}
+#endif
+
+Resistor::Resistor()
+{
+    R = alap_ertek;
+}
+
+Resistor::Resistor(double r)
+{
+    R = r;
+}
+
+Resistor Resistor::operator+ (const Resistor& r) const
+{
+    return Resistor(R + r.getR());
+}
+
+Resistor Resistor::operator% (const Resistor& r) const
+{
+    return Resistor(1/((1/R)+(1/r.getR())));
+}
+
+Resistor operator* (int n, const Resistor& r)
+{
+    if (n <= 0)
+        throw "F9ILEX";
+
+    return Resistor(n*r.getR());
+}
+
+//getI
+double Resistor::getI(double fesz)const{return fesz/R;}
+
+//getu
+double Resistor::getU(double arer)const{return arer * R;}
+
+//konstruktor
+Resistor::Resistor(const Resistor& rhs):R(rhs.R){}
+
+
+//destruktor
+Resistor :: ~Resistor(){}
+
+//egyenloseg op
+Resistor& Resistor::operator=(const Resistor& ellenallas){
+    R = ellenallas.R;
+    return *this;
+}
