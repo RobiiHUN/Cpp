cc9b6b07d9034b81621906a5d74e1a63fecf87bc
RobiiiHUN <58343667+RobiiHUN@users.noreply.github.com>
2024-04-17 16:19:57 +0200

    ilyen kzh szeruseg

--- kzh-2/2024.cpp
+++ kzh-2/2024.cpp
@@ -41,7 +41,7 @@ public:
         int *nData = new int[len + 1];
         for (size_t i = 1; i < len + 1; i++)
         {
-            nData[i]  = pData[i];
+            nData[i]  = pData[i - 1];
         }
         nData[0] = k;
         len++;
@@ -54,21 +54,28 @@ public:
         {
             nData[i]  = pData[i];
         }
+        int temp = pData[len];
+        nData[len] = pData[len - 1]; 
         len--;
         delete[] pData;
         pData = nData;
+        return temp;
         
     }
     int pop_front(){
         int *nData = new int[len - 1];
-        for (size_t i = 0; i < len; i++)
+        for (size_t i = 1; i < len; i++)
         {
-            nData[i]  = pData[i + 1];
+            nData[i - 1]  = pData[i];
         }
+        int temp = pData[0];
         len--;
         delete[] pData;
         pData = nData;
+        return temp;
     }
+
+    
     int& operator[](int idx)const{
         for (size_t i = 0; i < len; i++)
         {
@@ -94,7 +101,7 @@ int main(){
     //DinIntArray test(dinnar);
    // test.printDbg();
    // std::cout << test.size() << std::endl;
-   //dinnar.push_back(3);
+   dinnar.push_back(3);
    std::cout << dinnar[2] << std::endl;
 
 
